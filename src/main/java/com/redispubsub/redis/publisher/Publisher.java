7c83c24c85cc329529a845731a8400d6df53966b
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-06-11 16:07:27 -0300

    Initial commit

---
+++ src/main/java/com/redispubsub/redis/publisher/Publisher.java
@@ -0,0 +1,25 @@
+package com.redispubsub.redis.publisher;
+
+import com.redispubsub.redis.dto.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Publisher {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private ChannelTopic topic;
+
+    @PostMapping("/publish")
+    public String publish(@RequestBody Product product) {
+        redisTemplate.convertAndSend(topic.getTopic(), product.toString());
+        return "Event publish ok!";
+    }
+}
---
+++ src/main/java/com/redispubsub/redis/subscriber/Receiver.java
@@ -0,0 +1,16 @@
+package com.redispubsub.redis.subscriber;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+public class Receiver implements MessageListener {
+
+    Logger logger = LoggerFactory.getLogger(Receiver.class);
+
+    @Override
+    public void onMessage(Message message, byte[] bytes) {
+        logger.info("Consumed event {}", message);
+    }
+}
